6ce48db2073ac3aa20e0e3b5cb3656b75cd8a8d0
choridev <huicheol.yi@dsrvlabs.com>
2026-03-23 23:16:51 +0900

    Add taplo and marksman lspwq

--- nvim/nvim/lua/plugins/lsp.lua
+++ nvim/nvim/lua/plugins/lsp.lua
@@ -1,7 +1,7 @@
 return {
   "mason-org/mason-lspconfig.nvim",
   opts = {
-    ensure_installed = { "lua_ls", "yamlls", "gopls" },
+    ensure_installed = { "lua_ls", "yamlls", "taplo", "marksman", "gopls" },
   },
   dependencies = {
     {
