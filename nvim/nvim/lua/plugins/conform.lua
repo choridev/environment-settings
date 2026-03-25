4c71101e3909342a7be57c3fed5090b2d8450131
choridev <huicheol.yi@dsrvlabs.com>
2026-03-25 04:48:31 +0000

    Disable format on save feature by conform

--- nvim/nvim/lua/plugins/conform.lua
+++ nvim/nvim/lua/plugins/conform.lua
@@ -1,16 +1,16 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
-		conform.setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-			},
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
-		})
-	end,
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
+        conform.setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+            },
+            -- format_on_save = {
+            -- 	timeout_ms = 500,
+            -- 	lsp_fallback = true,
+            -- },
+        })
+    end,
 }
