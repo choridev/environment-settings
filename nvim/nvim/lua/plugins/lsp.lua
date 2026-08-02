a6f18c062367fe9adc14ef931270d4cb1789cc63
choridev <huicheol.yi@dsrvlabs.com>
2026-08-02 07:13:12 +0000

    Lazy-load plugins that are not needed during startup

--- nvim/nvim/lua/plugins/lsp.lua
+++ nvim/nvim/lua/plugins/lsp.lua
@@ -1,11 +1,13 @@
 return {
 	"mason-org/mason-lspconfig.nvim",
+	event = { "BufReadPre", "BufNewFile" }, -- no language server is needed until a buffer exists
 	opts = {
 		ensure_installed = { "lua_ls", "yamlls", "taplo", "marksman", "gopls" },
 	},
 	dependencies = {
 		{
 			"mason-org/mason.nvim",
+			cmd = "Mason",
 			opts = {
 				ui = {
 					icons = {
