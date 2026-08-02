964e177c8d5564a80b0a9bd4ac955824e9f590ff
choridev <huicheol.yi@dsrvlabs.com>
2026-08-02 07:13:12 +0000

    Load the telescope fzf extension and fold in ui-select

--- nvim/nvim/lua/plugins/telescope.lua
+++ nvim/nvim/lua/plugins/telescope.lua
@@ -1,26 +1,33 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		version = "*",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			-- optional but recommended
-			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-		},
-	},
-	{
+	"nvim-telescope/telescope.nvim",
+	version = "*",
+	-- Deferred to VeryLazy instead of `cmd = "Telescope"` so that ui-select still
+	-- takes over `vim.ui.select` without waiting for the first picker to open.
+	event = "VeryLazy",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		-- optional but recommended
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({
-							-- even more opts
-						}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
 	},
+	opts = function()
+		return {
+			extensions = {
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown({
+						-- even more opts
+					}),
+				},
+			},
+		}
+	end,
+	config = function(_, opts)
+		local telescope = require("telescope")
+		telescope.setup(opts)
+		telescope.load_extension("ui-select")
+		-- fzf-native needs a compiler; skip it rather than break startup.
+		if not pcall(telescope.load_extension, "fzf") then
+			vim.notify("telescope-fzf-native is unavailable; run :Lazy build telescope-fzf-native.nvim", vim.log.levels.WARN)
+		end
+	end,
 }
